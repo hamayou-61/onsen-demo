f2e4df25a7fc385e15367a485b5d5b8898b19239
hamayou-61 <caster2596@gmail.com>
2025-05-05 16:03:47 +0900

    パス修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/onsen-demo/',
+  // base: '/',
   plugins: [
     react(),
     tailwindcss()
