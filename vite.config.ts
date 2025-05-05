2efdf973e28d24074572aa80e2e9c2e44cd3eb6e
hamayou-61 <caster2596@gmail.com>
2025-05-05 16:18:07 +0900

    gh-pages削除

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/onsen-demo/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     react(),
     tailwindcss()
